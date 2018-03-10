bf25e44329802f6bb95cdb289206e3efc7434a2b
Tiofx <ioracle2718@gmail.com>
2018-03-10 10:08:36 +0300

    [add] method for comparison

---
+++ src/jobShop/state/comparison.go
@@ -0,0 +1,25 @@
+package state
+
+func (s State) Compare(second State) int {
+	min, max := s.EstimateTime()
+	min2, max2 := second.EstimateTime()
+
+	switch {
+
+	case min < min2:
+		fallthrough
+
+	case min == min2 && max < max2:
+		return 1
+
+	case min == min2 && max == max2:
+		return 0
+
+	default:
+		return -1
+	}
+}
+
+func (s State) IsBetterThan(second State) bool {
+	return s.Compare(second) == 1
+}
