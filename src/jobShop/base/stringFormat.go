9318c04ef6db6cf7acf212485a1e2f62b68c59e3
Tiofx <ioracle2718@gmail.com>
2018-03-10 09:47:55 +0300

    [add] method for representation base class as string

---
+++ src/jobShop/base/stringFormat.go
@@ -0,0 +1,28 @@
+package base
+
+import "fmt"
+
+func (jobs Jobs) String() (res string) {
+	for i, job := range jobs {
+		res += fmt.Sprintf("job %v: %v\n", i, job)
+	}
+
+	return
+}
+
+func (executed NumberOfExecutedTasks) String() (res string) {
+	//jobsToString([]int(executed), res)
+	for job, number := range executed {
+		res += fmt.Sprintf("job %v: %v\n", job, number)
+	}
+
+	return
+}
+
+func (left JobsTotalTime) String() (res string) {
+	for job, time := range left {
+		res += fmt.Sprintf("job %v: %v\n", job, time)
+	}
+
+	return
+}
