2b18b395109be2402b4db0d0aab4ceb76e8e111a
Tiofx <ioracle2718@gmail.com>
2018-03-12 10:29:43 +0300

    [change] test

--- src/jobShop/test/test.go
+++ src/jobShop/test/test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"jobShop/parser"
 	"jobShop/state"
+	"jobShop/tabuSearch"
 	"jobShop/initSolution/simpleGreed"
 )
 
@@ -27,18 +28,29 @@ func getAllFiles() []os.FileInfo {
 
 func TestAll() {
 	//for _, fileinfo := range getAllFiles() {
-	for _, fileinfo := range getAllFiles()[2:3] {
+	for _, fileinfo := range getAllFiles()[0:1] {
+		//for _, fileinfo := range getAllFiles()[2:3] {
 		fmt.Println(fileinfo.Name())
 		testCase := parser.NewTestCase(testDir + "/" + fileinfo.Name())
 
-		//state := taskWaveByMachineGreed.Resolver{state.NewState(testCase.Jobs)}
-		state := simpleGreed.Resolver{State: state.NewState(testCase.Jobs)}
+		//state := taskWaveByMachineGreed.Resolver{State: state.NewState(testCase.Jobs)}
+		initState := simpleGreed.Resolver{State: state.NewState(testCase.Jobs)}
 
-		solution := state.FindSolution()
+		solution := initState.FindSolution()
 
 		fmt.Println(testCase.Optimum)
 		result := solution.Makespan()
 		fmt.Println(result)
+		newState := solution.(simpleGreed.Resolver).State
+		//newState := solution.(taskWaveByMachineGreed.Resolver).State
+
+		fmt.Println("first: ", solution.Makespan())
+		solver := tabuSearch.NewSolver(newState)
+		for i := 0; i < 200; i++ {
+			solver.Next()
+		}
+		fmt.Println(solver.BestMakespan())
+
 		fmt.Println(1.0 - float32(testCase.Optimum)/float32(result))
 		fmt.Println(float32(result) / float32(testCase.Optimum))
 		fmt.Println("------")
