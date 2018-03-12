320fc480de7c5e30735bc13d26fcad514487f1b0
Tiofx <ioracle2718@gmail.com>
2018-03-12 10:26:13 +0300

    [add] methods for determinate critical path

--- src/jobShop/tabuSearch/tabuSearch.go
+++ src/jobShop/tabuSearch/tabuSearch.go
@@ -147,6 +147,40 @@ func (graph DisjunctiveGraph) To(jobs base.Jobs) (jobState *state.State, exist b
 	return jobState, true
 }
 
+func criticalJob(state state.State) job {
+	var (
+		criticalJob       int
+		timeOfCriticalJob int
+	)
+
+	for job, time := range state.JobTimeWave {
+		if time > timeOfCriticalJob {
+			criticalJob = job
+			timeOfCriticalJob = time
+		}
+	}
+
+	return job(criticalJob)
+}
+
+type taskPosition int
+type criticalTasks map[base.Machine][]taskPosition
+
+func (graph DisjunctiveGraph) criticalTaskPositionFor(criticalJob job) criticalTasks {
+	tasks := make(criticalTasks)
+
+	for machine, jobOrder := range graph {
+		for index, job := range jobOrder {
+			if job == criticalJob {
+				machine := base.Machine(machine)
+				tasks[machine] = append(tasks[machine], taskPosition(index))
+			}
+		}
+	}
+
+	return tasks
+}
+
 		}
 	}
 
