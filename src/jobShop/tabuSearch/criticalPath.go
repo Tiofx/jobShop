09124f22836912fd165af74295ad41e03ec8185c
Tiofx <ioracle2718@gmail.com>
2018-03-13 11:32:47 +0300

    [move] critical job determination logic

---
+++ src/jobShop/tabuSearch/criticalPath.go
@@ -0,0 +1,42 @@
+package tabuSearch
+
+import (
+	"jobShop/base"
+	"jobShop/state"
+)
+
+type taskPosition int
+
+type criticalTasks map[base.Machine][]taskPosition
+
+
+func criticalJob(state state.State) job {
+	var (
+		criticalJob       int
+		timeOfCriticalJob int
+	)
+
+	for job, time := range state.JobTimeWave {
+		if time > timeOfCriticalJob {
+			criticalJob = job
+			timeOfCriticalJob = time
+		}
+	}
+
+	return job(criticalJob)
+}
+
+func (graph DisjunctiveGraph) criticalTaskPositionFor(criticalJob job) criticalTasks {
+	tasks := make(criticalTasks)
+
+	for machine, jobOrder := range graph {
+		for index, job := range jobOrder {
+			if job == criticalJob {
+				machine := base.Machine(machine)
+				tasks[machine] = append(tasks[machine], taskPosition(index))
+			}
+		}
+	}
+
+	return tasks
+}
--- src/jobShop/tabuSearch/tabuSearch.go
+++ src/jobShop/tabuSearch/tabuSearch.go
@@ -115,41 +115,6 @@ func (graph DisjunctiveGraph) To(jobs base.Jobs) (jobState *state.State, exist b
 
 	return jobState, true
 }
-
-func criticalJob(state state.State) job {
-	var (
-		criticalJob       int
-		timeOfCriticalJob int
-	)
-
-	for job, time := range state.JobTimeWave {
-		if time > timeOfCriticalJob {
-			criticalJob = job
-			timeOfCriticalJob = time
-		}
-	}
-
-	return job(criticalJob)
-}
-
-type taskPosition int
-type criticalTasks map[base.Machine][]taskPosition
-
-func (graph DisjunctiveGraph) criticalTaskPositionFor(criticalJob job) criticalTasks {
-	tasks := make(criticalTasks)
-
-	for machine, jobOrder := range graph {
-		for index, job := range jobOrder {
-			if job == criticalJob {
-				machine := base.Machine(machine)
-				tasks[machine] = append(tasks[machine], taskPosition(index))
-			}
-		}
-	}
-
-	return tasks
-}
-
 func (graph DisjunctiveGraph) neighbours(tasks criticalTasks) (res []DisjunctiveGraph) {
 
 	for machine, tasksOfMachine := range tasks {
