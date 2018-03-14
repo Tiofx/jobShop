267ba1f8ba6eaf7eca8297cbb74b7dce3bd6413b
Tiofx <ioracle2718@gmail.com>
2018-03-14 23:25:40 +0300

    [add] neighborhood by longest job

---
+++ src/jobShop/tabuSearch/neighborhood/byLongestJob.go
@@ -0,0 +1,40 @@
+package neighborhood
+
+import (
+	"jobShop/state"
+	"jobShop/tabuSearch/graph_state"
+)
+
+type ByLongestJob struct{ byJob }
+
+func NewByLongestJob(jobState *state.State, graph *graph_state.DisjunctiveGraph) ByLongestJob {
+	return ByLongestJob{
+		byJob: byJob{
+			JobState: jobState,
+			Graph:    graph,
+		},
+	}
+}
+
+func (r ByLongestJob) criticalJob() job {
+	var (
+		criticalJob       int
+		timeOfCriticalJob int
+	)
+
+	for job, time := range r.JobState.JobTimeWave {
+		if time > timeOfCriticalJob {
+			criticalJob = job
+			timeOfCriticalJob = time
+		}
+	}
+
+	return job(criticalJob)
+}
+
+func (r ByLongestJob) Generator() (iterator <-chan Move) {
+	criticalJob := r.criticalJob()
+	tasks := r.taskPositionFor(criticalJob)
+
+	return r.generateFor(criticalJob, tasks)
+}
