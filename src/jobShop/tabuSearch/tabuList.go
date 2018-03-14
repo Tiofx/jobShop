2da0632867c6b612efba81df76d5c8e722f78e43
Tiofx <ioracle2718@gmail.com>
2018-03-14 08:30:15 +0300

    [change] tabu list struct
    [add] implementation of interface TabuList

--- src/jobShop/tabuSearch/tabuList.go
+++ src/jobShop/tabuSearch/tabuList.go
@@ -5,4 +5,20 @@ type TabuList interface {
 	Add(move move)
 }
 
-type tabuList neighboursSet
+type tabuList struct {
+	list []move
+}
+
+func (tabu *tabuList) Contain(move move) bool {
+	for _, tabuMove := range tabu.list {
+		if move == tabuMove {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (tabu *tabuList) Add(move move) {
+	tabu.list = append(tabu.list, move)
+}
