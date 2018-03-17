cda77d7b13fa7cb7a6238efef47e2e1430fe5c19
Tiofx <ioracle2718@gmail.com>
2018-03-17 17:50:55 +0300

    [add] measure of test execution time

--- src/jobShop/test/testResult.go
+++ src/jobShop/test/testResult.go
@@ -4,11 +4,13 @@ import (
 	"jobShop/base"
 	"jobShop/state"
 	"fmt"
+	"time"
 )
 
 type Result struct {
 	Problem ProblemDescription
 	Result  state.State
+	Elapsed time.Duration
 }
 
 type ProblemDescription struct{ testCase }
@@ -21,7 +23,8 @@ func (r Result) String() (res string) {
 	res += fmt.Sprintf("%v", r.Problem)
 	res += fmt.Sprintf("expected makespan: %v\n", r.Problem.Optimum)
 	res += fmt.Sprintf("actual makespan:   %v\n", r.Result.Makespan())
-	res += fmt.Sprintf("result is worse then expeted by %.2f%%\n", r.HowMuchWorse()*100)
+	res += fmt.Sprintf("result is worse then expeted value by %.2f%%\n", r.HowMuchWorse()*100)
+	res += fmt.Sprintf("elapsed time is %v\n", r.Elapsed)
 
 	return
 }
@@ -31,8 +34,15 @@ func (r Result) HowMuchWorse() float32 {
 }
 
 func test(testCase testCase, solver func(jobs base.Jobs) state.State) Result {
+	start := time.Now()
+
+	result := solver(testCase.Jobs)
+
+	duration := time.Since(start)
+
 	return Result{
 		Problem: ProblemDescription{testCase},
-		Result:  solver(testCase.Jobs),
+		Result:  result,
+		Elapsed: duration,
 	}
 }
