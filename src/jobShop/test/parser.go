a835c64390218e6ded7015af9e6280f2987d12e7
Tiofx <ioracle2718@gmail.com>
2018-03-17 11:06:45 +0300

    [add] String method for test case

--- src/jobShop/test/parser.go
+++ src/jobShop/test/parser.go
@@ -7,19 +7,32 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"fmt"
 )
 
 type testCase struct {
+	Filename                         string
 	JobsNumber, TasksNumber, Optimum int
 
 	base.Jobs
 }
 
+func (ts *testCase) String() (res string) {
+	res += fmt.Sprintf("----- Short test description -----")
+	res += fmt.Sprintf("file name: %v", ts.Filename)
+	res += fmt.Sprintf("job number: %v", ts.JobsNumber)
+	res += fmt.Sprintf("task number on each job: %v", ts.TasksNumber)
+	res += fmt.Sprintf("optimum: %v", ts.Optimum)
+	res += fmt.Sprintf("---------------------")
+	return
+}
+
 func newTestCase(filename string) testCase {
-	scanner := newTestParser(filename)
+	scanner := newTestParser(fullPathTo(filename))
 	jobsNumber, taskNumbers, optimum, jobs := scanner.parseAllData()
 
 	return testCase{
+		Filename:    filename,
 		JobsNumber:  jobsNumber,
 		TasksNumber: taskNumbers,
 		Optimum:     optimum,
@@ -60,6 +73,7 @@ func newTestParser(filename string) *testParser {
 
 	return &testParser{Scanner: scanner}
 }
+
 func skipLine(scanner *bufio.Scanner) {
 	scanner.Scan()
 }
