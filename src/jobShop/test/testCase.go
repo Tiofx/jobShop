4ab1420360d9e8db926d3faddff6cd7a17c78721
Tiofx <ioracle2718@gmail.com>
2018-03-20 15:06:20 +0300

    [change] result format

--- src/jobShop/test/testCase.go
+++ src/jobShop/test/testCase.go
@@ -27,10 +27,12 @@ func newTestCase(filename string) testCase {
 
 func (ts testCase) String() (res string) {
 	res += fmt.Sprintf("----- Short test description -----\n")
-	res += fmt.Sprintf("file name:   %v\n", ts.Filename)
-	res += fmt.Sprintf("job number:  %v\n", ts.JobsNumber)
-	res += fmt.Sprintf("task number: %v\n", ts.TasksNumber)
-	res += fmt.Sprintf("optimum:     %v\n", ts.Optimum)
+	res += fmt.Sprintf("file name:			%v\n", ts.Filename)
+	res += fmt.Sprintf("job number:  		%v\n", ts.JobsNumber)
+	res += fmt.Sprintf("avg. task number:	%v\n", float64(ts.TotalTaskNumber()/ts.JobsNumber))
+	res += fmt.Sprintf("total task number: 	%v\n", ts.TotalTaskNumber())
+	res += fmt.Sprintf("machine number:		%v\n", ts.MachineNumber())
+	res += fmt.Sprintf("optimum:     		%v\n", ts.Optimum)
 	res += fmt.Sprintf("----------------------------------\n")
 	return
 }
