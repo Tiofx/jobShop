2a7fa590fe9789bd9e9e1d4d2d1b35c6f707bebb
Tiofx <ioracle2718@gmail.com>
2018-03-20 20:38:54 +0300

    [add] neighborhood by all possible move

---
+++ src/jobShop/tabuSearch/neighborhood/byAll.go
@@ -0,0 +1,31 @@
+package neighborhood
+
+import (
+	"jobShop/state"
+	"jobShop/tabuSearch/graph_state"
+)
+
+type ByAll struct{ byJob }
+
+func NewByAll(jobState *state.State, graph *graph_state.DisjunctiveGraph) ByAll {
+	return ByAll{
+		byJob: byJob{
+			JobState: jobState,
+			Graph:    graph,
+		},
+	}
+}
+
+func (r ByAll) Generate() []Move {
+	var res []Move
+
+	for machine, jobList := range (*r.Graph) {
+		for i, jobI := range jobList {
+			for _, jobJ := range jobList[i+1:] {
+				res = append(res, Move{Machine: int(machine), I: int(jobI), J: int(jobJ)})
+			}
+		}
+	}
+
+	return res
+}
