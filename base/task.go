ad63bd2ad2f1830e234436f43a725bba5e9a1639
Tiofx <ioracle2718@gmail.com>
2018-05-10 23:07:03 +0300

    [add] struct tag of json to base.Task struct
    lowercase name of fields

--- base/task.go
+++ base/task.go
@@ -1,10 +1,12 @@
 package base
 
 type Task struct {
-	Machine Machine
-	Time    int
+	Machine Machine `json:"machine"`
+	Time    int     `json:"time"`
 }
 
 func (t Task) Deconstruct() (Machine, int) {
 	return t.Machine, t.Time
 }
+
+
